f141836892d0f67803ba93d43e3b4f8714f7a7ba
deisynok <deisynok@gmail.com>
2025-06-25 22:37:34 -0400

    Create app.py

---
+++ app.py
@@ -0,0 +1,6 @@
+import streamlit as st
+
+st.set_page_config(page_title="BioCurate", layout="centered")
+
+st.title("📦 BioCurate")
+st.write("Este será o app de leitura de códigos de exsicatas do HUAM.")
