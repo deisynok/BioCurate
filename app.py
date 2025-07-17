0421271434f8d8be6aaf23e0542ed91e352bc6f1
deisynok <deisynok@gmail.com>
2025-07-16 21:58:29 -0400

    Add files via upload

--- app.py
+++ app.py
@@ -393,7 +393,7 @@ elif selected == "Busca":
             collected = f"{number or ''}".strip()
             if coll or collected:
                 st.markdown(
-                    f"<b>Coletor:</b> {coll or ''} nº {collected} & {addcoll or ''}",
+                    f"<b>Coletor(s):</b> {coll or ''} <b>nº</b> {collected} <b>&</b> {addcoll or ''}",
                     unsafe_allow_html=True
                 )
 
